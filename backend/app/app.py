20d9584fad90d54d99a711e2317893c7ce0e2772
Thiago-FR <thiago_ssfr@hotmail.com>
2023-03-23 14:17:15 -0300

    fix: add gunicorn

--- backend/app/app.py
+++ backend/app/app.py
@@ -3,9 +3,16 @@ from .routes.user_router import user
 from flask_cors import CORS
 
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-    app.register_blueprint(user)
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)
+#     app.register_blueprint(user)
 
-    return app
+#     return app
+
+app = Flask(__name__)
+CORS(app)
+app.register_blueprint(user)
+
+if __name__ == "__main__":
+    app.run(debug=True)
